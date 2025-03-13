2f14c8702e796b15da0610ae46fc559f8fe91c6b
marchelucie <lucie.pouponneau@live.fr>
2025-03-13 11:36:21 +0100

    Debug boutons de la galerie
    Navigation entre les images corrigée

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -153,8 +153,8 @@
           index = i ;
         }
       });
-      next =
-        imagesCollection[index] ||
+      next =  
+        imagesCollection[index - 1] ||  
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
